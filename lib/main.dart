ec8fffee914d59d8e1427ba6b808fe50875446da
seoljaehyeok <milkboy2564@naver.com>
2021-01-22 21:58:54 +0900

    기존 코드 => provider적용해서 다시 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/providers/products.dart';
 import 'package:flutter_shop_app/screens/product_detail_screen.dart';
 import 'package:flutter_shop_app/screens/product_overview_screen.dart';
+
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
-
-
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    final products = productData.item;
+    return GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: products.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 3 / 2,
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (ctx, index) => ProductItem(
+              products[index].id,
+              products[index].title,
+              products[index].imageUrl,
+            ));
+  }
+}
