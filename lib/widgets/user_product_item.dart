eef250a4700e9b2fd717768658d8d16419d86c52
seoljaehyeok <milkboy2564@naver.com>
2021-01-27 13:20:39 +0900

    Delete 버튼 기능구현

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/providers/cart.dart';
+import 'package:flutter_shop_app/providers/products.dart';
 import 'package:flutter_shop_app/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -26,13 +27,17 @@ class UserProductItem extends StatelessWidget {
                 icon: Icon(Icons.edit),
                 color: Theme.of(context).primaryColor,
                 onPressed: () {
-                  Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+                  Navigator.of(context)
+                      .pushNamed(EditProductScreen.routeName, arguments: id);
                 },
               ),
               IconButton(
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () {},
+                onPressed: () {
+                  Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                },
               ),
             ],
           ),
