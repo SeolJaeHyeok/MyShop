2cd8e22208eba19fc1c04f50eea95ac37204984e
seoljaehyeok <milkboy2564@naver.com>
2021-02-02 20:46:15 +0900

    새로운 URL로 바꿔서 작성

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,9 +8,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url =
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB7OocKPhQCxXIUvfujeacLk0gHVifLZaY';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB7OocKPhQCxXIUvfujeacLk0gHVifLZaY';
 
     final response = await http.post(
       url,
@@ -23,5 +24,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
